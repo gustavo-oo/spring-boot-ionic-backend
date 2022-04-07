84717aeaf5e8a342edea0284221e5aa5e8557e6a
gustavo-oo <gustavopchaves@hotmail.com>
2022-04-07 17:32:24 -0300

    Adicionado método delete

--- src/main/java/com/gustavo/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/gustavo/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.gustavo.cursomc.services.exceptions.DataIntegrityException;
 import com.gustavo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -17,11 +18,24 @@ public class ResourceExceptionHandler {
 											ObjectNotFoundException exception, 
 											HttpServletRequest request){
 		
-		StandardError error = new StandardError(
+		StandardError error = new StandardError(	
 									HttpStatus.NOT_FOUND.value(), 
 									exception.getMessage(), 
 									System.currentTimeMillis());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity( //Recebe como parâmetro um objectNotFound
+											DataIntegrityException exception, 
+											HttpServletRequest request){
+		
+		StandardError error = new StandardError(
+									HttpStatus.BAD_REQUEST.value(), 
+									exception.getMessage(), 
+									System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
