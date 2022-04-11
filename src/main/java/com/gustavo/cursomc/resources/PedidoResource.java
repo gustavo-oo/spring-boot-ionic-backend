e3944b563b85ed48bafcee45902fbd812a3ec15b
gustavo-oo <gustavopchaves@hotmail.com>
2022-04-11 14:46:43 -0300

    Inserindo Pedido

--- src/main/java/com/gustavo/cursomc/resources/PedidoResource.java
+++ src/main/java/com/gustavo/cursomc/resources/PedidoResource.java
@@ -1,13 +1,22 @@
 package com.gustavo.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gustavo.cursomc.domain.Cliente;
 import com.gustavo.cursomc.domain.Pedido;
+import com.gustavo.cursomc.dto.ClienteNewDTO;
 import com.gustavo.cursomc.services.PedidoService;
 
 @RestController
@@ -23,5 +32,20 @@ public class PedidoResource {
 		Pedido obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		
+		obj = service.insert(obj);
+		
+		//Gera o url para o pedido criado
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(obj.getId())
+					.toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/gustavo/cursomc/services/PedidoService.java
+++ src/main/java/com/gustavo/cursomc/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.gustavo.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gustavo.cursomc.domain.ItemPedido;
+import com.gustavo.cursomc.domain.PagamentoComBoleto;
 import com.gustavo.cursomc.domain.Pedido;
+import com.gustavo.cursomc.domain.Produto;
+import com.gustavo.cursomc.domain.enums.EstadoPagamento;
+import com.gustavo.cursomc.repositories.ItemPedidoRepository;
+import com.gustavo.cursomc.repositories.PagamentoRepository;
 import com.gustavo.cursomc.repositories.PedidoRepository;
 import com.gustavo.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -13,12 +20,55 @@ import com.gustavo.cursomc.services.exceptions.ObjectNotFoundException;
 public class PedidoService {
 	
 	@Autowired
-	private PedidoRepository repo;
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepositoy;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private ProdutoService produtoService;
 	
 	public Pedido find(Integer id) {
-		Optional<Pedido> obj = repo.findById(id);
+		Optional<Pedido> obj = pedidoRepository.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = pedidoRepository.save(obj);
+		
+		pagamentoRepositoy.save(obj.getPagamento());
+		
+		Produto produto;
+		
+		for(ItemPedido item: obj.getItens()) {
+			produto = produtoService.find(item.getProduto().getId());
+					
+			item.setDesconto(0.0);
+			item.setPreco(produto.getPreco());
+			item.setPedido(obj);
+			item.setProduto(produto);
+		}
+		
+		itemPedidoRepository.saveAll(obj.getItens());
+		
+		return obj;
 	}
 }
